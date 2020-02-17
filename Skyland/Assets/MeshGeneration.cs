280f6cdc59919d2f4c6b6bc40446a729e6cde9e0
LarsFasil <fasil.school@gmail.com>
2020-02-17 03:36:54 +0100

    init

---
+++ Skyland/Assets/MeshGeneration.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGeneration : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] v3A_vertices;
+    int[] iA_triangles;
+
+    MeshCollider meshcollider;
+
+    [Range(0.0f, 10.0f)]
+    public float perlinMultiplier, mountainMult;
+    [Range(0.0f, 1.0f)]
+    public float perlinreductor;
+    [Range(0, 100)]
+    public int mountainPercentage;
+
+    public bool refresh = false;
+    public bool gizmos = false;
+    int i_sizeX, i_sizeY, i_chunkX, i_chunkY;
+
+    public float yield;
+
+    public void Init(int sx, int sy, int cx, int cy, float scale)
+    {
+        i_sizeX = sx;
+        i_sizeY = sy;
+
+        i_chunkX = cx;
+        i_chunkY = cy;
+
+        mountainMult = 0;
+
+        transform.localScale = new Vector3(scale, scale, scale);
+
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        meshcollider = GetComponent<MeshCollider>();
+
+        CreateMesh();
+        UpdateMesh();
+    }
+
+    void CreateMesh()
+    {
+        v3A_vertices = new Vector3[(i_sizeX + 1) * (i_sizeY + 1)];
+        float p;
+        float emX = 0;
+        float emY = 0;
+        long i = 0;
+
+        float percentageX = i_sizeX / 100 * mountainPercentage;
+        float percentageY = i_sizeY / 100 * mountainPercentage;
+
+        for (int y = 0 + (i_sizeY*i_chunkY); y <= i_sizeY + (i_sizeY * i_chunkY); y++)
+        {
+            for (int x = 0 + (i_sizeX * i_chunkX); x <= i_sizeX + +(i_sizeX * i_chunkX); x++)
+            {
+                //if (x <= percentageX || x >= (i_sizeX - percentageX) || y <= percentageY || y >= (i_sizeY - percentageY))
+                //{
+                //    em = mountainMult;
+                //}
+
+                if (x < percentageX)
+                {
+                    emX = ((x - percentageX) * -1) / percentageX * mountainMult;
+                }
+                if (x > (i_sizeX - percentageX))
+                {
+                    emX = (x - (i_sizeX - percentageX)) / percentageX * mountainMult;
+                }
+
+
+                if (y < percentageY)
+                {
+                    emY = ((y - percentageY) * -1) / percentageY * mountainMult;
+                }
+                if (y > (i_sizeY - percentageY))
+                {
+                    emY = (y - (i_sizeY - percentageY)) / percentageY * mountainMult;
+                }
+
+                if (emX <= emY)
+                {
+                    emX = emY;
+                }
+
+                if (emX < 1)
+                {
+                    emX = 1;
+                }
+                p = Mathf.PerlinNoise(x * perlinreductor, y * perlinreductor) * (emX * perlinMultiplier) * (perlinMultiplier); //* (Mathf.Sin(Time.time) + 1));
+                v3A_vertices[i] = new Vector3(x, p, y);
+                emX = 0;
+                emY = 0;
+                i++;
+
+                //yield return new WaitForSeconds(yield);
+            }
+            //Debug.Log(i);
+        }
+
+        iA_triangles = new int[i_sizeX * i_sizeY * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int y = 0; y < i_sizeY; y++)
+        {
+            for (int x = 0; x < i_sizeX; x++)
+            {
+                iA_triangles[tris + 0] = vert + 0;
+                iA_triangles[tris + 1] = vert + 1 + i_sizeX;
+                iA_triangles[tris + 2] = vert + 1;
+
+                iA_triangles[tris + 3] = vert + 1;
+                iA_triangles[tris + 4] = vert + 1 + i_sizeX;
+                iA_triangles[tris + 5] = vert + 2 + i_sizeX;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = v3A_vertices;
+        mesh.triangles = iA_triangles;
+
+        mesh.RecalculateNormals();
+
+        meshcollider.sharedMesh = mesh;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (gizmos)
+        {
+            if (v3A_vertices == null)
+            {
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < v3A_vertices.Length; i++)
+                {
+                    Gizmos.DrawSphere(v3A_vertices[i], .1f);
+                }
+            }
+        }
+
+    }
+}
---
+++ Skyland/Assets/TerrainManager.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+    public GameObject go_dessert1;
+    public Transform tf_player, tf_chunkParent;
+
+    public int i_terrainSizeX;
+    public int i_terrainSizeY;
+    public float f_scale;
+
+    public Vector2Int v2_playerChunk;
+    GameObject[] goA_playerZone;
+
+    Dictionary<Vector2Int, GameObject> dict_VisitedChunks = new Dictionary<Vector2Int, GameObject>();
+
+
+    void Start()
+    {
+        v2_playerChunk = new Vector2Int(0, 0);
+        goA_playerZone = new GameObject[9];
+
+        MakeInitialZone();
+    }
+
+    void Update()
+    {
+        TrackPlayer();
+    }
+
+    void Move(short dir)
+    {
+        int[] North = { 0, 1, 2 };
+        int[] East = { 0, 3, 6 };
+        int[] South = { 6, 7, 8 };
+        int[] West = { 2, 5, 8 };
+
+        switch (dir)
+        {
+            case 1:
+                UpdatePlayerZone(North, North.Length);
+                break;
+            case 2:          
+                UpdatePlayerZone(East, 1);
+                break;
+            case 3:
+                UpdatePlayerZone(South, -South.Length);
+                break;
+            case 4:
+                UpdatePlayerZone(West, -1);
+                break;
+        }
+        LoadDir(dir);
+    }
+
+    void UpdatePlayerZone(int[] code, int increment)
+    {
+        for (int i = 0; i < code.Length; i++)
+        {
+            goA_playerZone[code[i]].SetActive(false);
+            goA_playerZone[code[i]] = goA_playerZone[code[i] + increment];
+            goA_playerZone[code[i] + increment] = goA_playerZone[code[i] + (increment * 2)];
+        }
+    }
+
+    void TrackPlayer()
+    {
+        float ppX = tf_player.position.x;
+        float ppY = tf_player.position.z;
+
+        if (ppY > (v2_playerChunk.y + 1) * i_terrainSizeY * f_scale)
+        {
+            // North
+            v2_playerChunk.y++;
+            Move(1);
+        }
+        if (ppX > (v2_playerChunk.x + 1) * i_terrainSizeX * f_scale)
+        {
+            // East
+            v2_playerChunk.x++;
+            Move(2);
+        }
+        if (ppY < v2_playerChunk.y * i_terrainSizeY * f_scale)
+        {
+            // South
+            v2_playerChunk.y--;
+            Move(3);
+        }
+        if (ppX < (v2_playerChunk.x) * i_terrainSizeX * f_scale)
+        {
+            // West
+            v2_playerChunk.x--;
+            Move(4);
+        }
+    }
+
+    void MakeInitialZone()
+    {
+        //int initialChunkGridSize = 5;
+        
+        //// Initialize first x amount of chunks
+        //for (int y = -initialChunkGridSize; y < initialChunkGridSize; y++)
+        //{
+        //    for (int x = -initialChunkGridSize; x < initialChunkGridSize; x++)
+        //    {
+        //        InstandGO(x, y).SetActive(false);
+        //    }
+        //}
+
+        goA_playerZone[3] = InstandGO(-1, 0);
+        goA_playerZone[4] = InstandGO(0, 0);
+        goA_playerZone[5] = InstandGO(1, 0);
+
+        LoadDir(1);
+        LoadDir(3);
+    }
+
+    GameObject InstandGO(int x, int y)
+    {
+        Vector2Int chunk = new Vector2Int(x, y);
+        if (!dict_VisitedChunks.ContainsKey(chunk))
+        {
+            GameObject mesh = Instantiate(go_dessert1, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            mesh.GetComponent<MeshGeneration>().Init(i_terrainSizeX, i_terrainSizeY, x, y, f_scale);
+
+            // Clean up Hierarchy
+            mesh.transform.parent = tf_chunkParent;
+
+            // Update List
+            dict_VisitedChunks.Add(chunk, mesh);
+
+            return mesh;
+        }
+
+        if (!dict_VisitedChunks[chunk].activeSelf)
+        {
+            dict_VisitedChunks[chunk].SetActive(true);
+        }
+        return dict_VisitedChunks[chunk];
+    }
+
+    void LoadDir(int dir)
+    {
+        switch (dir)
+        {
+            case 1:
+                goA_playerZone[6] = InstandGO(v2_playerChunk.x - 1, v2_playerChunk.y + 1);
+                goA_playerZone[7] = InstandGO(v2_playerChunk.x, v2_playerChunk.y + 1);
+                goA_playerZone[8] = InstandGO(v2_playerChunk.x + 1, v2_playerChunk.y + 1);
+                break;
+
+            case 2:
+                goA_playerZone[2] = InstandGO(v2_playerChunk.x + 1, v2_playerChunk.y - 1);
+                goA_playerZone[5] = InstandGO(v2_playerChunk.x + 1, v2_playerChunk.y);
+                goA_playerZone[8] = InstandGO(v2_playerChunk.x + 1, v2_playerChunk.y + 1);
+                break;
+
+            case 3:
+                goA_playerZone[0] = InstandGO(v2_playerChunk.x - 1, v2_playerChunk.y - 1);
+                goA_playerZone[1] = InstandGO(v2_playerChunk.x, v2_playerChunk.y - 1);
+                goA_playerZone[2] = InstandGO(v2_playerChunk.x + 1, v2_playerChunk.y - 1);
+                break;
+
+            case 4:
+                goA_playerZone[0] = InstandGO(v2_playerChunk.x - 1, v2_playerChunk.y - 1);
+                goA_playerZone[3] = InstandGO(v2_playerChunk.x - 1, v2_playerChunk.y);
+                goA_playerZone[6] = InstandGO(v2_playerChunk.x - 1, v2_playerChunk.y + 1);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
